d4f0af632383885d2671d28fb704ddabdcc522e0
John Peel <john@dgby.org>
2018-10-07 12:34:04 -0400

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return 'Hello Jazy!'
 
 if __name__ == '__main__':
     app.run(debug=False, host='0.0.0.0')
