4a1c6a18164dba8de9cb1f8390f0d1dbfbd6e8b4
John Peel <john@dgby.org>
2018-10-23 20:16:05 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,16 @@
 from flask import Flask
+
+try:
+    import cx_Oracle
+    cx_Oracle_test = 'SUCCESS'
+except ImportError:
+    cx_Oracle_test = 'FAILED'
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello Jazy!'
+    return 'Hello Jazy! ' + cx_Oracle_test
 
 if __name__ == '__main__':
     app.run(debug=False, host='0.0.0.0')
